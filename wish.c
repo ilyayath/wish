fdb668aad872af0d41de1f6ede27d85fc59916df
Ilya Yathenko <yathenkoilya2005@gmail.com>
2026-09-17 13:41:00 +0000

    read line from input

--- wish.c
+++ wish.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    char *line = NULL;
+    size_t line_size = 0;
+
+    while (1) {
+        printf("wish> ");
+        fflush(stdout);
+
+        if (getline(&line, &line_size, stdin) == -1) {
+            break;
+        }
+
+        printf("ти ввів: %s", line);
+
+        free(line);
+        return 0;
+    }
+
     printf("hello\n");
     return 0;
 }
