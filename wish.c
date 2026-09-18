80cf21e7821101177319ef548125f9af2f912d4d
Ilya Yathenko <yathenkoilya2005@gmail.com>
2026-09-18 11:20:55 +0000

    builtin exit and cd

--- wish.c
+++ wish.c
@@ -6,16 +6,13 @@
 
 #define MAX_ARGS 128
 
-// каталоги, у яких шукаємо програми
 char *shell_path[MAX_ARGS];
 int shell_path_size = 0;
 
-// шукаємо команду в усіх каталогах path
-// повертає повний шлях (його треба звільнити) або NULL, якщо не знайшли
 char *find_program(char *command)
 {
     for (int i = 0; i < shell_path_size; i++) {
-        // +2 це символ '/' і '\0'
+        // +2 це '/' і '\0'
         char *full = malloc(strlen(shell_path[i]) + strlen(command) + 2);
 
         strcpy(full, shell_path[i]);
@@ -23,7 +20,7 @@ char *find_program(char *command)
         strcat(full, command);
 
         if (access(full, X_OK) == 0) {
-            return full;   // знайшли, і файл можна виконати
+            return full;
         }
 
         free(full);
@@ -36,7 +33,6 @@ int main(void)
     char *line = NULL;
     size_t line_size = 0;
 
-    // на початку шукаємо програми тільки в /bin
     shell_path[0] = strdup("/bin");
     shell_path_size = 1;
 
@@ -45,10 +41,9 @@ int main(void)
         fflush(stdout);
 
         if (getline(&line, &line_size, stdin) == -1) {
-            break;   // кінець вводу (Ctrl+D)
+            break;
         }
 
-        // розбиваємо рядок на окремі слова
         char *args[MAX_ARGS];
         int args_count = 0;
 
@@ -57,7 +52,7 @@ int main(void)
 
         while ((token = strsep(&rest, " \t\n")) != NULL) {
             if (strcmp(token, "") == 0) {
-                continue;   // між двома пробілами strsep дає порожній токен
+                continue;   // strsep дає порожні токени
             }
             args[args_count] = token;
             args_count++;
@@ -65,7 +60,26 @@ int main(void)
         args[args_count] = NULL;   // execv вимагає NULL в кінці
 
         if (args_count == 0) {
-            continue;   // порожній рядок, просто Enter
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0) {
+            if (args_count != 1) {
+                printf("exit не приймає аргументів\n");
+                continue;
+            }
+            exit(0);
+        }
+
+        if (strcmp(args[0], "cd") == 0) {
+            if (args_count != 2) {
+                printf("cd приймає рівно один аргумент\n");
+                continue;
+            }
+            if (chdir(args[1]) != 0) {
+                printf("немає такого каталогу\n");
+            }
+            continue;
         }
 
         char *program = find_program(args[0]);
@@ -79,13 +93,11 @@ int main(void)
         if (pid < 0) {
             printf("fork не вдався\n");
         } else if (pid == 0) {
-            // ми в дочірньому процесі
             execv(program, args);
             // сюди потрапляємо тільки якщо execv не спрацював
             printf("execv не вдався\n");
             exit(1);
         } else {
-            // ми в батьківському процесі
             waitpid(pid, NULL, 0);
             free(program);
         }
